9b9caab799b981da17154268f5db5d732a51ec8e
Nikita Pintikov <m.pintikov@student.csn.khai.edu>
2021-10-06 12:12:19 +0300

    Resolved conflict
    Test.java added to feature_branch with change on string #2

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,7 @@
 public class Test {
     public static void main(String[] args) {
-
-        System.out.println("Java");
-
-        System.out.println("education");
- 
+        System.out.println("Java education");
+        //git status
+        System.out.println("DICT&quot");
     }
 }
